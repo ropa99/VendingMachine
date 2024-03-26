261af4fe5008d7ee0ebc4b81bbf3449532c79f27
ropa99 <rpahlin@hotmail.com>
2024-03-26 09:36:17 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/VendingMachine/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.VendingMachine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
